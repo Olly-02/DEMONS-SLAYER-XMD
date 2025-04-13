6ccbe3331980dc4f717365d4d29cc72de4c100ea
Olly-02 <oluwaseunolamilekan522@gmail.com>
2025-04-13 23:28:46 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -27,7 +27,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false,
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "private",
-  OWNER_NAME: process.env.OWNER_NAME || "Marisel",
+  OWNER_NAME: process.env.OWNER_NAME || "Olly",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "2347045509716",
   SUDO_NUMBER: process.env.SUDO_NUMBER || "2347045509716",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
